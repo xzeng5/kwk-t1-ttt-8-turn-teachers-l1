f2bf23168f34d66102ad22cc183c0157d386f35c
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-01 17:44:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,11 +8,12 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  def input_to_index (user_input)
+    user_input.to_i - 1
+  end
 end
 
-def input_to_index (user_input)
-  user_input.to_i - 1
-end
+
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
