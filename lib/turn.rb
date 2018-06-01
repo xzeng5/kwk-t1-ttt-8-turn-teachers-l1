b51c4d4e887895da871205795864afa7dd3ff9a3
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-01 16:17:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,17 +15,17 @@ def input_to_index (user_input)
   index - 1
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+  end
+end
+
 def valid_move?(board, index)
   if position_taken? == false && index.between(0, 8)
     true
   else
     false
-  end
-end
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
   end
 end
